756148b92aef9b66c8b8a61b60dcbdd66acd46c7
ncohensm <ncohensm@cisco.com>
2013-10-10 13:08:29 -0700

    debug newrelic startup

--- apache-tomcat-7.0.42/bin/catalina.sh
+++ apache-tomcat-7.0.42/bin/catalina.sh
@@ -235,6 +235,10 @@ fi
 # org.apache.catalina.security.SecurityListener
 #JAVA_OPTS="$JAVA_OPTS -Dorg.apache.catalina.security.SecurityListener.UMASK=`umask`"
 
+pwd
+ls
+find / -name newrelic.jar
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 JAVA_OPTS="$JAVA_OPTS -javaagent:$DIR/../newrelic/newrelic.jar"
 echo $JAVA_OPTS
